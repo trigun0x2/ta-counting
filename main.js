98351a2b63fea98ead2fddd9dd51f8d2a19c3dbf
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-06 10:28:28 -0800

    i am dumb

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var ans = null;
 var triviaState = false;
 TA.twitch.chat.on('say', function(data){
   console.log(data.message);
-  if (data.message == '!trivia' && triviaState){
+  if (data.message == '!trivia' && !triviaState){
     $.get('http://jservice.io/api/random', function(trivia){
       TA.twitch.chat.say(trivia[0].question);
       ans = trivia[0].answer;
