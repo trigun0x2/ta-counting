156e8b1b4cf43924d5cfc254745b93266af03b22
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-14 15:37:44 -0800

    Debounce messages and also only check for integers

--- src/js/app.js
+++ src/js/app.js
@@ -8,23 +8,25 @@ class TwitchCounts {
     this.bufferSize = 3;
 
     TA.twitch.chat.on('say', (data) => {
-      if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
-        this.currentNumber += 1;
-        this.buffer = 0;
-      } else if (data.message == this.currentNumber && this.buffer <= this.bufferSize){
-        this.buffer += 1;
-      } else {
-        this.currentNumber = 0;
-        this.buffer = 0;
-        TA.twitch.chat.say("Reset, try again 4Head");
+      if (!isNaN(parseInt(data.message))){
+        if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
+          this.currentNumber += 1;
+          this.buffer = 0;
+        } else if (data.message == this.currentNumber && this.buffer <= this.bufferSize){
+          this.buffer += 1;
+        } else {
+          this.currentNumber = 0;
+          this.buffer = 0;
+          _.debounce(TA.twitch.chat.say("Reset, try again 4Head"), 1500);
+        }
+        $("#current-number").text(this.currentNumber);
+        if (this.currentNumber == this.maxNumber){
+          TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
+          this.currentNumber = 0;
+          this.buffer = 0;
+        }
+        console.log(this.currentNumber);
       }
-      $("#current-number").text(this.currentNumber);
-      if (this.currentNumber == this.maxNumber){
-        TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
-        this.currentNumber = 0;
-        this.buffer = 0;
-      }
-      console.log(this.currentNumber);
     });
   };
 
