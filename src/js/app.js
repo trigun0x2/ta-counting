a4086e46636168b15e88e0a952130b505e183936
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-14 14:56:27 -0800

    fixed counting

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,13 @@ var TA = window.TA;
 
 class TwitchCounts {
   constructor() {
-    this.currentNumber = 1;
+    this.currentNumber = 0;
     this.maxNumber = 10;
     this.buffer = 0;
     this.bufferSize = 3;
 
     TA.twitch.chat.on('say', (data) => {
-      if (this.currentNumber == this.maxNumber){
-        TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
-      } else if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
+      if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
         this.currentNumber += 1;
       } else if (data.message == this.currentNumber && this.buffer <= this.bufferSize){
         this.buffer += 1;
@@ -19,6 +17,10 @@ class TwitchCounts {
         TA.twitch.chat.say("Reset, try again 4Head");
       }
       $("#current-number").text(this.currentNumber);
+      if (this.currentNumber == this.maxNumber){
+        TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
+        this.currentNumber = 1;
+      }
       console.log(this.currentNumber);
     });
   };
