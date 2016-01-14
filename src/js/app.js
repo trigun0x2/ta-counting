40b93ef0d3467216e83fc84d25eeef5ac85368b1
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-14 15:11:59 -0800

    reset buffer please!

--- src/js/app.js
+++ src/js/app.js
@@ -10,16 +10,19 @@ class TwitchCounts {
     TA.twitch.chat.on('say', (data) => {
       if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
         this.currentNumber += 1;
+        this.buffer = 0;
       } else if (data.message == this.currentNumber && this.buffer <= this.bufferSize){
         this.buffer += 1;
       } else {
         this.currentNumber = 0;
+        this.buffer = 0;
         TA.twitch.chat.say("Reset, try again 4Head");
       }
       $("#current-number").text(this.currentNumber);
       if (this.currentNumber == this.maxNumber){
         TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
         this.currentNumber = 0;
+        this.buffer = 0;
       }
       console.log(this.currentNumber);
     });
