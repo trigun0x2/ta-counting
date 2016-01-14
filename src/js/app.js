c7ae83c92550ca9c07f44871bbfc1a3ffeb8bdc5
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-14 13:14:55 -0800

    don't need TA.init

--- src/js/app.js
+++ src/js/app.js
@@ -7,21 +7,19 @@ class TwitchCounts {
     this.buffer = 0;
     this.bufferSize = 3;
 
-    TA.init(() => {
-      TA.twitch.chat.on('say', (data) => {
-        if (this.currentNumber == this.maxNumber){
-          TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
-        } else if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
-          this.currentNumber += 1;
-        } else if (data.message == this.currentNumber && this.buffer <= this.bufferSize){
-          this.buffer += 1;
-        } else {
-          this.currentNumber = 1;
-          TA.twitch.chat.say("Reset, try again 4Head");
-        }
-        $("#current-number").text(this.currentNumber);
-        console.log(this.currentNumber);
-      });
+    TA.twitch.chat.on('say', (data) => {
+      if (this.currentNumber == this.maxNumber){
+        TA.twitch.chat.say("HOLY CRAP YOU DID IT CHAT!! PogChamp PogChamp");
+      } else if (data.message == (this.currentNumber + 1).toString() && this.buffer <= this.bufferSize){
+        this.currentNumber += 1;
+      } else if (data.message == this.currentNumber && this.buffer <= this.bufferSize){
+        this.buffer += 1;
+      } else {
+        this.currentNumber = 1;
+        TA.twitch.chat.say("Reset, try again 4Head");
+      }
+      $("#current-number").text(this.currentNumber);
+      console.log(this.currentNumber);
     });
   };
 
